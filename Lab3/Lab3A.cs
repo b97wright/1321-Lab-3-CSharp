48ced8af69e3ff2d5d5bdccc56f5dcf3462ef9aa
rarelambo21 <35502394+rarelambo21@users.noreply.github.com>
2022-07-29 14:16:34 -0400

    Lab3A Lab3B

---
+++ Lab3/Lab3A.cs
@@ -0,0 +1,37 @@
+﻿/*
+    Class: 1321L
+    Section: Section W#1 C#
+    Term: Spring 2022
+    Instructor: Gayathri Yella
+    Name: Brandon Wright
+    Lab#: 3A
+ */
+
+using System;
+
+namespace Lab3
+{
+    class Lab3A
+    {
+        static void Main(string[] args)
+        {
+            string uI;
+
+            double ammountOwed, APR, minimumPayment, monthlyPer;
+
+            Console.Write("Amount owed: $");
+            uI = Console.ReadLine();
+            ammountOwed = Convert.ToDouble(uI);
+
+            Console.Write("APR: ");
+            uI = Console.ReadLine();
+            APR = Convert.ToDouble(uI);
+
+            minimumPayment = ammountOwed * (APR / 12) * .01;
+            monthlyPer = APR / 12;
+
+            Console.WriteLine("Monthly percentage rate: " + Math.Round(monthlyPer, 6));
+            Console.WriteLine("Minimum payment: $" + Math.Round(minimumPayment, 5));
+        }
+    }
+}
---
+++ Lab3/Lab3B.cs
@@ -0,0 +1,74 @@
+﻿/*
+    Class: 1321L
+    Section: Section W#1 C#
+    Term: Spring 2022
+    Instructor: Gayathri Yella
+    Name: Brandon Wright
+    Lab#: 3B
+*/
+
+using System;
+
+namespace Lab3
+{
+    class Lab3B
+    {
+        static void Main(string[] args)
+        {
+            string uI;
+
+            double credits, hours, qualityPoints, totalHours, GPA;
+
+            Console.Write("Course 1 hours: ");
+            uI = Console.ReadLine();
+            hours = Convert.ToInt32(uI);
+
+            Console.Write("Grade for course 1: ");
+            uI = Console.ReadLine();
+            credits = Convert.ToInt32(uI);
+
+            qualityPoints = hours * credits;
+            totalHours = hours;
+
+            Console.Write("Course 2 hours: ");
+            uI = Console.ReadLine();
+            hours = Convert.ToInt32(uI);
+
+            Console.Write("Grade for course 2: ");
+            uI = Console.ReadLine();
+            credits = Convert.ToInt32(uI);
+
+            qualityPoints = qualityPoints + (hours * credits);
+            totalHours = totalHours + hours;
+
+            Console.Write("Course 3 hours: ");
+            uI = Console.ReadLine();
+            hours = Convert.ToInt32(uI);
+
+            Console.Write("Grade for course 3: ");
+            uI = Console.ReadLine();
+            credits = Convert.ToInt32(uI);
+
+            qualityPoints = qualityPoints + (hours * credits);
+            totalHours = totalHours + hours;
+
+            Console.Write("Course 4 hours: ");
+            uI = Console.ReadLine();
+            hours = Convert.ToInt32(uI);
+
+            Console.Write("Grade for course 4: ");
+            uI = Console.ReadLine();
+            credits = Convert.ToInt32(uI);
+
+            qualityPoints = qualityPoints + (hours * credits);
+            totalHours = totalHours + hours;
+
+            Console.WriteLine("Total hours is: " + totalHours);
+            Console.WriteLine("Total quality points is: " + qualityPoints);
+
+            GPA = qualityPoints / totalHours;
+
+            Console.WriteLine("Your GPA for this semester is " + Math.Round(GPA, 5));
+        }
+    }
+}
